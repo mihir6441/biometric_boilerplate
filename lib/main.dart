f44dbe958353096e078e6e47366e08de0429c695
mihirbhojani <mvbhojani007@gmail.com>
2023-06-19 18:00:37 +0530

    Initial commit by mihir on 19-06-2023

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:biometric_demo/pages/fingerprint_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  static const String title = 'Local Auth';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: title,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const FingerprintPage(),
+    );
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,46 @@
+import 'package:biometric_demo/main.dart';
+import 'package:biometric_demo/pages/fingerprint_page.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(MyApp.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(32),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Home',
+                style: TextStyle(fontSize: 40),
+              ),
+              const SizedBox(height: 48),
+              buildLogoutButton(context)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildLogoutButton(BuildContext context) => ElevatedButton(
+    style: ElevatedButton.styleFrom(
+      minimumSize: const Size.fromHeight(50),
+    ),
+    child: const Text(
+      'Logout',
+      style: TextStyle(fontSize: 20),
+    ),
+    onPressed: () => Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (context) => const FingerprintPage()),
+    ),
+  );
+
+}
